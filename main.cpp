6fd065bd2bdedf3d11c6f6716c8be6ee1f0b3394
jordi <jordiwave@gmail.com>
2023-03-15 20:51:22 +0100

    Added a new line

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 	cout << "Hello World!" << endl;
 	cout << "I just added this line" << endl;
 	cout << "Added a new line with VSCodium" << endl;
+	cout << "This line added to show the staging area" << endl;
 
 	return 0;
 }
